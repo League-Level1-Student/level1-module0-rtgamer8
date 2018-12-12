8d6b83733d0c69749a878737f76cde6ef90bf575
rtgamer8 <league@WTS22.attlocal.net>
2018-12-11 18:27:09 -0800

    My first swing Photo quiz incomplete
      Finished My first swing worked on Photo quiz incomplete

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -1,8 +1,8 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 1
  */
-
 
 import java.awt.Component;
 import java.net.MalformedURLException;
@@ -12,32 +12,35 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 
 public class PhotoQuiz {
 
 	public static void main(String[] args) throws Exception {
 		JFrame quizWindow = new JFrame();
 		quizWindow.setVisible(true);
-                quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
+		quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // This will make sure the program exits when you
+																	// close the window
 
-		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
+		// 1. find an image on the internet, and put its URL in a String variable (from
+		// your browser, right click on the image, and select “Copy Image Address”)
 
 		// 2. create a variable of type "Component" that will hold your image
-
+		Component c = createImage("//images-na.ssl-images-amazon.com/images/I/61uSJJQwBvL._SX425_.jpg");
 		// 3. use the "createImage()" method below to initialize your Component
-
-		// 4. add the image to the quiz window
+		
+			// 4. add the image to the quiz window
+quizWindow.add(c); 
 
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+JOPtion
 		// 7. print "CORRECT" if the user gave the right answer
 
 		// 8. print "INCORRECT" if the answer is wrong
 
-		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
+		// 9. remove the component from the quiz window (you may not see the effect of
+		// this until step 12)
 
 		// 10. find another image and create it (might take more than one line of code)
 
@@ -60,10 +63,6 @@ public class PhotoQuiz {
 
 	/* OPTIONAL */
 	// *14. add scoring to your quiz
-	// *15. make something happen when mouse enters image (imageComponent.addMouseMotionListener()) 
+	// *15. make something happen when mouse enters image
+	// (imageComponent.addMouseMotionListener())
 }
-
-
-
-
-
